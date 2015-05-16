d503c5c0e56eb875676b59e9c538f27d439acbde
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2015-05-17 01:11:39 +0300

    Linux compatability fixes

--- src/SocketHelper.h
+++ src/SocketHelper.h
@@ -2,21 +2,25 @@
 #define SOCKET_HELPER_H
 
 	#include "SocketException.h"
-
+        #include <string.h>
+	
 	#ifdef _WIN32
 		#pragma comment(lib, "Ws2_32.lib")
 		#include <io.h>
 		#include <winsock2.h>
 		#include <ws2tcpip.h>
 	#else
+                #include <netdb.h>
+		#include <unistd.h>
 		#include <sys/socket.h>
+		#include <netinet/in.h>
 		typedef int SOCKET;
 	#endif
 
 	class SocketHelper {
 		public:
-			static void SocketHelper::closeSocket(SOCKET socket);
-			static sockaddr_in SocketHelper::createSocketInfo(char* hostname, int port);
+			static void closeSocket(SOCKET socket);
+			static sockaddr_in createSocketInfo(char* hostname, int port);
 	};
 
 #endif
